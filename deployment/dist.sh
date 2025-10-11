5ccaf6d09a7d95bd66c2418b0dcce4d8687ee54f
Alex Tants <aleks_dev@mlp-co.dev>
2025-10-11 21:31:48 +0300

    Improve install apps

--- deployment/dist.sh
+++ deployment/dist.sh
@@ -45,12 +45,15 @@ upload() {
 # Can be sourced from different scripts
 source ./deployment-commands.sh 2>/dev/null || true
 
+local supressConnectionOutput="2>&1 | grep -v 'Connection to'"
+
 # Install Docker and Docker Compose (if not already installed)
 # Usage:
 # install_docker
 install_docker() {
   remote "command -v docker >/dev/null 2>&1 || \
-    { curl -fsSL https://get.docker.com | sh && systemctl enable --now docker; }"
+    { curl -fsSL https://get.docker.com | sh && systemctl enable --now docker; }; \
+    $supressConnectionOutput"
 }
 
 # install_docker_compose() {
@@ -62,7 +65,8 @@ install_docker() {
 # install_nvm
 install_nvm() {
   remote "command -v nvm >/dev/null 2>&1 || \
-    { curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash && source \$HOME/.bash_profile; }"
+    { curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash && source \$HOME/.bash_profile; }; \
+    $supressConnectionOutput"
 }
 
 # Usage:
@@ -71,7 +75,8 @@ install_nodejs() {
   install_nvm
   local node_version="${1:-18}"
 
-  remote "nvm install $node_version || nvm use $node_version"
+  remote "nvm install $node_version || nvm use $node_version; \
+  $supressConnectionOutput"
 }
 
   
