d5e0656cd39f89507d7ffab0804c6e3bb16f9232
Alex Tants <aleks_dev@mlp-co.dev>
2025-12-14 21:51:35 +0200

    Build

--- deployment/dist.sh
+++ deployment/dist.sh
@@ -17,7 +17,7 @@ set +o allexport
 remote() {
   local sources="~/.bashrc ~/.profile ~/.nvm/nvm.sh ~/.bash_profile"
   local applySources="for f in $sources; do [ -f \$f ] && source \$f > /dev/null 2>&1; done;"
-  local command="bash -lc '$applySources $@'"
+  local command="bash -lc \"$applySources $@\""
   
   if [ -z "$REMOTE_HOST" ]; then
     bash -c "$command" 2>&1 | grep -v 'Connection to'
@@ -30,12 +30,12 @@ remote() {
 
 # Upload files to remote server (with progress)
 # Usage:
-# upload_files <source> <destination> -- [additional rsync options]
+# upload <source> <destination> -- [additional rsync options]
 upload() {
   local source="$1"
   local destination="$2"
   shift 2
-  rsync -chavzP --stats -e "ssh -i $SSH_KEY" "$@" "$source" root@${REMOTE_HOST}:"$destination"
+  rsync -avP --stats -e "ssh -i $SSH_KEY" "$@" "$source" root@${REMOTE_HOST}:"$destination"
 }
 
 # Download files from remote server (with progress)
@@ -45,7 +45,7 @@ download() {
   local source="$1"
   local destination="$2"
   shift 2
-  rsync -chavzP --stats -e "ssh -i $SSH_KEY" "$@" root@${REMOTE_HOST}:"$source" "$destination"
+  rsync -avP --stats -e "ssh -i $SSH_KEY" "$@" root@${REMOTE_HOST}:"$source" "$destination"
 }
 
 # ============================================================================
